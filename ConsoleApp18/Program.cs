4db94d25a913f50a9d8cb6f1c2ee621d9ab5edeb
NHOY <NHOY@2210DR000919>
2023-05-05 01:04:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heroName;
+            int heroLevel;
+            uint resourcesMoney;
+            byte heroLevelUpPoint;
+            ushort numberObjectsOnScreen;
+            short heroScreenCoordinate;
+            float levelBattle;
+            bool haveHeroIsLife;
+            char reducingNumberLevelHero;
+            long totalAmountResourcesInInventory;
+        }
+    }
+}
